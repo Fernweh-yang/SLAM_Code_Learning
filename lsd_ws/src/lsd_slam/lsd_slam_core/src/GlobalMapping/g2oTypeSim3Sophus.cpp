51f79878b99ff225f1cd4c11c0db67ca9f25646d
Fernweh-yang <512127058@qq.com>
2023-11-15 21:47:05 +0100

    图优化顶点和边

--- lsd_ws/src/lsd_slam/lsd_slam_core/src/GlobalMapping/g2oTypeSim3Sophus.cpp
+++ lsd_ws/src/lsd_slam/lsd_slam_core/src/GlobalMapping/g2oTypeSim3Sophus.cpp
@@ -25,111 +25,111 @@
 
 namespace lsd_slam
 {
-G2O_USE_TYPE_GROUP(sba);
+    G2O_USE_TYPE_GROUP(sba);
 
-G2O_REGISTER_TYPE_GROUP(sim3sophus);
+    G2O_REGISTER_TYPE_GROUP(sim3sophus);
 
-G2O_REGISTER_TYPE(VERTEX_SIM3_SOPHUS : EXPMAP, VertexSim3);
-G2O_REGISTER_TYPE(EDGE_SIM3_SOPHUS : EXPMAP, EdgeSim3);
+    G2O_REGISTER_TYPE(VERTEX_SIM3_SOPHUS : EXPMAP, VertexSim3);
+    G2O_REGISTER_TYPE(EDGE_SIM3_SOPHUS : EXPMAP, EdgeSim3);
 
-VertexSim3::VertexSim3() : g2o::BaseVertex<7, Sophus::Sim3d>()
-{
-  _marginalized = false;
-  _fix_scale = false;
-}
+    VertexSim3::VertexSim3() : g2o::BaseVertex<7, Sophus::Sim3d>()
+    {
+        _marginalized = false;
+        _fix_scale = false;
+    }
 
-bool VertexSim3::write(std::ostream& os) const
-{
-  // TODO
-  assert(false);
-  return false;
-  //     Sim3 cam2world(estimate().inverse());
-  //     Vector7d lv=cam2world.log();
-  //     for (int i=0; i<7; i++){
-  //       os << lv[i] << " ";
-  //     }
-  //     for (int i=0; i<2; i++)
-  //     {
-  //       os << _focal_length[i] << " ";
-  //     }
-  //     for (int i=0; i<2; i++)
-  //     {
-  //       os << _principle_point[i] << " ";
-  //     }
-  //     return os.good();
-}
+    bool VertexSim3::write(std::ostream &os) const
+    {
+        // TODO
+        assert(false);
+        return false;
+        //     Sim3 cam2world(estimate().inverse());
+        //     Vector7d lv=cam2world.log();
+        //     for (int i=0; i<7; i++){
+        //       os << lv[i] << " ";
+        //     }
+        //     for (int i=0; i<2; i++)
+        //     {
+        //       os << _focal_length[i] << " ";
+        //     }
+        //     for (int i=0; i<2; i++)
+        //     {
+        //       os << _principle_point[i] << " ";
+        //     }
+        //     return os.good();
+    }
 
-bool VertexSim3::read(std::istream& is)
-{
-  // TODO
-  assert(false);
-  return false;
-  //     Vector7d cam2world;
-  //     for (int i=0; i<6; i++){
-  //       is >> cam2world[i];
-  //     }
-  //     is >> cam2world[6];
-  // //    if (! is) {
-  // //      // if the scale is not specified we set it to 1;
-  // //      std::cerr << "!s";
-  // //      cam2world[6]=0.;
-  // //    }
-  //
-  //     for (int i=0; i<2; i++)
-  //     {
-  //       is >> _focal_length[i];
-  //     }
-  //     for (int i=0; i<2; i++)
-  //     {
-  //       is >> _principle_point[i];
-  //     }
-  //
-  //     setEstimate(Sim3(cam2world).inverse());
-  //     return true;
-}
+    bool VertexSim3::read(std::istream &is)
+    {
+        // TODO
+        assert(false);
+        return false;
+        //     Vector7d cam2world;
+        //     for (int i=0; i<6; i++){
+        //       is >> cam2world[i];
+        //     }
+        //     is >> cam2world[6];
+        // //    if (! is) {
+        // //      // if the scale is not specified we set it to 1;
+        // //      std::cerr << "!s";
+        // //      cam2world[6]=0.;
+        // //    }
+        //
+        //     for (int i=0; i<2; i++)
+        //     {
+        //       is >> _focal_length[i];
+        //     }
+        //     for (int i=0; i<2; i++)
+        //     {
+        //       is >> _principle_point[i];
+        //     }
+        //
+        //     setEstimate(Sim3(cam2world).inverse());
+        //     return true;
+    }
 
-EdgeSim3::EdgeSim3() : g2o::BaseBinaryEdge<7, Sophus::Sim3d, VertexSim3, VertexSim3>()
-{
-}
+    EdgeSim3::EdgeSim3() : g2o::BaseBinaryEdge<7, Sophus::Sim3d, VertexSim3, VertexSim3>()
+    {
+    }
 
-bool EdgeSim3::write(std::ostream& os) const
-{
-  // TODO
-  assert(false);
-  return false;
-  //     Sim3 cam2world(measurement().inverse());
-  //     Vector7d v7 = cam2world.log();
-  //     for (int i=0; i<7; i++)
-  //     {
-  //       os  << v7[i] << " ";
-  //     }
-  //     for (int i=0; i<7; i++)
-  //       for (int j=i; j<7; j++){
-  //         os << " " <<  information()(i,j);
-  //     }
-  //     return os.good();
-}
+    bool EdgeSim3::write(std::ostream &os) const
+    {
+        // TODO
+        assert(false);
+        return false;
+        //     Sim3 cam2world(measurement().inverse());
+        //     Vector7d v7 = cam2world.log();
+        //     for (int i=0; i<7; i++)
+        //     {
+        //       os  << v7[i] << " ";
+        //     }
+        //     for (int i=0; i<7; i++)
+        //       for (int j=i; j<7; j++){
+        //         os << " " <<  information()(i,j);
+        //     }
+        //     return os.good();
+    }
 
-bool EdgeSim3::read(std::istream& is)
-{
-  // TODO
-  assert(false);
-  return false;
-  //     Vector7d v7;
-  //     for (int i=0; i<7; i++){
-  //       is >> v7[i];
-  //     }
-  //
-  //     Sim3 cam2world(v7);
-  //     setMeasurement(cam2world.inverse());
-  //
-  //     for (int i=0; i<7; i++)
-  //       for (int j=i; j<7; j++)
-  //       {
-  //         is >> information()(i,j);
-  //         if (i!=j)
-  //           information()(j,i)=information()(i,j);
-  //       }
-  //     return true;
-}
-}  // namespace lsd_slam
+    bool EdgeSim3::read(std::istream &is)
+    {
+        // TODO
+        assert(false);
+        return false;
+        //     Vector7d v7;
+        //     for (int i=0; i<7; i++){
+        //       is >> v7[i];
+        //     }
+        //
+        //     Sim3 cam2world(v7);
+        //     setMeasurement(cam2world.inverse());
+        //
+        //     for (int i=0; i<7; i++)
+        //       for (int j=i; j<7; j++)
+        //       {
+        //         is >> information()(i,j);
+        //         if (i!=j)
+        //           information()(j,i)=information()(i,j);
+        //       }
+        //     return true;
+    }
+} // namespace lsd_slam
--- lsd_ws/src/lsd_slam/lsd_slam_core/src/GlobalMapping/g2oTypeSim3Sophus.h
+++ lsd_ws/src/lsd_slam/lsd_slam_core/src/GlobalMapping/g2oTypeSim3Sophus.h
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with LSD-SLAM. If not, see <http://www.gnu.org/licenses/>.
  */
-
+// ! 这个文件定义了g2o图优化所需要的顶点g2o::BaseVertex<7, Sophus::Sim3d> 和 边g2o::BaseBinaryEdge<7, Sophus::Sim3d, VertexSim3, VertexSim3>
 #pragma once
 #include "util/SophusUtil.h"
 
@@ -29,102 +29,124 @@
 
 namespace lsd_slam
 {
-class VertexSim3 : public g2o::BaseVertex<7, Sophus::Sim3d>
-{
-public:
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
-  VertexSim3();
-  virtual bool read(std::istream& is);
-  virtual bool write(std::ostream& os) const;
+    // 重写顶点：顶点的维度为7，数据类型是sophus中定义的sim3d
+    // Sim(3)三维相似变换矩阵：因为单目相机的尺度不确定，所以
+    class VertexSim3 : public g2o::BaseVertex<7, Sophus::Sim3d>
+    {
+    public:
+        // EIGEN的宏，确保操作符分配的内存是对齐的
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+        
+        // 构造函数
+        VertexSim3();
+        
+        // read 和 write 函数分别用于从输入流读取顶点信息和将顶点信息写入输出流。
+        virtual bool read(std::istream &is);
+        virtual bool write(std::ostream &os) const;
+        
+        // 顶点的重置函数
+        virtual void setToOriginImpl()
+        {
+            _estimate = Sophus::Sim3d();    // 要被优化的估计值的初始值设为Sim3d()，其中每个元素为0
+        }
 
-  virtual void setToOriginImpl()
-  {
-    _estimate = Sophus::Sim3d();
-  }
+        // 顶点的更新函数,也就是位姿的更新
+        virtual void oplusImpl(const double *update_)
+        {
+            // const_cast用于去除update_变量的常量修饰符const
+            // 从而允许将其存储到非常量变量：update
+            Eigen::Map<Eigen::Matrix<double, 7, 1>> update(const_cast<double *>(update_));
 
-  virtual void oplusImpl(const double* update_)
-  {
-    Eigen::Map<Eigen::Matrix<double, 7, 1> > update(const_cast<double*>(update_));
+            if (_fix_scale)
+                update[6] = 0;  // 如果_fix_scale为真，将第7个元素也就是尺度设置为0
 
-    if (_fix_scale)
-      update[6] = 0;
+            // Sophus::Sim3d::exp(update) 将增量变换转换为sim3变换
+            // 然后乘以当前的估计值：estimate()
+            // 最后通过setEstimate()将结果设置为新的估计值 
+            setEstimate(Sophus::Sim3d::exp(update) * estimate());
+        }
 
-    setEstimate(Sophus::Sim3d::exp(update) * estimate());
-  }
+        bool _fix_scale;
+    };
 
-  bool _fix_scale;
-};
+    /**
+     * \brief 7D edge between two Vertex7
+     */
+    // 重写边：主要写自己的误差模型
+    class EdgeSim3 : public g2o::BaseBinaryEdge<7, Sophus::Sim3d, VertexSim3, VertexSim3>
+    {
+    public:
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+        // 构造函数
+        EdgeSim3();
 
-/**
- * \brief 7D edge between two Vertex7
- */
-class EdgeSim3 : public g2o::BaseBinaryEdge<7, Sophus::Sim3d, VertexSim3, VertexSim3>
-{
-public:
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  EdgeSim3();
+        // read 和 write 函数分别用于从输入流读取顶点信息和将顶点信息写入输出流。
+        virtual bool read(std::istream &is);
+        virtual bool write(std::ostream &os) const;
 
-  virtual bool read(std::istream& is);
-  virtual bool write(std::ostream& os) const;
+        // 重写误差函数
+        void computeError()
+        {   
+            // 第一个顶点，位姿0
+            const VertexSim3 *_from = static_cast<const VertexSim3 *>(_vertices[0]);
+            // 第二个顶点，位姿1
+            const VertexSim3 *_to = static_cast<const VertexSim3 *>(_vertices[1]);
+            // 计算误差：位姿1的逆 * 位姿2 * 边的逆
+            // ? 类似于14讲271页公式10.4 
+            Sophus::Sim3d error_ = _from->estimate().inverse() * _to->estimate() * _inverseMeasurement;
+            _error = error_.log();
+        }
 
-  void computeError()
-  {
-    const VertexSim3* _from = static_cast<const VertexSim3*>(_vertices[0]);
-    const VertexSim3* _to = static_cast<const VertexSim3*>(_vertices[1]);
+        // 雅可比矩阵计算
+        void linearizeOplus()
+        {
+            const VertexSim3 *_from = static_cast<const VertexSim3 *>(_vertices[0]);
 
-    Sophus::Sim3d error_ = _from->estimate().inverse() * _to->estimate() * _inverseMeasurement;
-    _error = error_.log();
-  }
+            _jacobianOplusXj = _from->estimate().inverse().Adj();
+            _jacobianOplusXi = -_jacobianOplusXj;
+        }
 
-  void linearizeOplus()
-  {
-    const VertexSim3* _from = static_cast<const VertexSim3*>(_vertices[0]);
+        virtual void setMeasurement(const Sophus::Sim3d &m)
+        {
+            _measurement = m;
+            _inverseMeasurement = m.inverse();
+        }
 
-    _jacobianOplusXj = _from->estimate().inverse().Adj();
-    _jacobianOplusXi = -_jacobianOplusXj;
-  }
+        virtual bool setMeasurementData(const double *m)
+        {
+            // Eigen::Map<g2o::Vector7> test;
+            Eigen::Map<const g2o::Vector7> v(m);
+            setMeasurement(Sophus::Sim3d::exp(v));
+            return true;
+        }
 
-  virtual void setMeasurement(const Sophus::Sim3d& m)
-  {
-    _measurement = m;
-    _inverseMeasurement = m.inverse();
-  }
+        virtual bool setMeasurementFromState()
+        {
+            const VertexSim3 *from = static_cast<const VertexSim3 *>(_vertices[0]);
+            const VertexSim3 *to = static_cast<const VertexSim3 *>(_vertices[1]);
+            Sophus::Sim3d delta = from->estimate().inverse() * to->estimate();
+            setMeasurement(delta);
+            return true;
+        }
 
-  virtual bool setMeasurementData(const double* m)
-  {
-    // Eigen::Map<g2o::Vector7> test;
-    Eigen::Map<const g2o::Vector7> v(m);
-    setMeasurement(Sophus::Sim3d::exp(v));
-    return true;
-  }
+        virtual double initialEstimatePossible(const g2o::OptimizableGraph::VertexSet &, g2o::OptimizableGraph::Vertex *)
+        {
+            return 1.;
+        }
 
-  virtual bool setMeasurementFromState()
-  {
-    const VertexSim3* from = static_cast<const VertexSim3*>(_vertices[0]);
-    const VertexSim3* to = static_cast<const VertexSim3*>(_vertices[1]);
-    Sophus::Sim3d delta = from->estimate().inverse() * to->estimate();
-    setMeasurement(delta);
-    return true;
-  }
+        virtual void initialEstimate(const g2o::OptimizableGraph::VertexSet &from, g2o::OptimizableGraph::Vertex * /*to*/)
+        {
+            VertexSim3 *_from = static_cast<VertexSim3 *>(_vertices[0]);
+            VertexSim3 *_to = static_cast<VertexSim3 *>(_vertices[1]);
 
-  virtual double initialEstimatePossible(const g2o::OptimizableGraph::VertexSet&, g2o::OptimizableGraph::Vertex*)
-  {
-    return 1.;
-  }
+            if (from.count(_from) > 0)
+                _to->setEstimate(_from->estimate() * _measurement);
+            else
+                _from->setEstimate(_to->estimate() * _inverseMeasurement);
+        }
 
-  virtual void initialEstimate(const g2o::OptimizableGraph::VertexSet& from, g2o::OptimizableGraph::Vertex* /*to*/)
-  {
-    VertexSim3* _from = static_cast<VertexSim3*>(_vertices[0]);
-    VertexSim3* _to = static_cast<VertexSim3*>(_vertices[1]);
+    protected:
+        Sophus::Sim3d _inverseMeasurement;
+    };
 
-    if (from.count(_from) > 0)
-      _to->setEstimate(_from->estimate() * _measurement);
-    else
-      _from->setEstimate(_to->estimate() * _inverseMeasurement);
-  }
-
-protected:
-  Sophus::Sim3d _inverseMeasurement;
-};
-
-}  // namespace lsd_slam
+} // namespace lsd_slam
